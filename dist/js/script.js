0d19d5cd7631e8411b097d5a9b0520b8ef070a17
Firman155 <firmannur0105@gmail.com>
2023-02-05 21:40:48 +0700

    fitur kirim form ke email

--- dist/js/script.js
+++ dist/js/script.js
@@ -19,3 +19,30 @@ hamburger.addEventListener('click', function(){
     navMenu.classList.toggle('hidden');
 });
 
+
+// pesan
+const contactForm = document.getElementById("contact-form");
+const loader = document.querySelector(".loader");
+
+loader.style.display = "none";
+
+
+contactForm.addEventListener("submit", function(e){
+    loader.style.display = "block";
+    e.preventDefault();
+
+    const url = e.target.action;
+    const formData = new FormData(contactForm);
+
+    fetch(url,{
+        method: "POST",
+        body: formData,
+        mode: "no-cors",
+    })
+    .then(()=>{
+        // url thank you
+        loader.style.display = "none";
+        window.location.href = '/thankyou.html';
+    })
+    .catch((e)=>alert("Error Occured"));
+});
