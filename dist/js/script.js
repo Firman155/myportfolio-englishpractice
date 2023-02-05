840ee50937af9e83949eade4ec5dc0bc47bf90d6
Firman155 <firmannur0105@gmail.com>
2023-02-05 18:46:28 +0700

    portfolio tailwind css

---
+++ dist/js/script.js
@@ -0,0 +1,21 @@
+// Navbar Fixed
+window.onscroll = function(){
+    const header = document.querySelector('header');
+    const FixedNav= header.offsetTop;
+
+    if(window.pageYOffset>FixedNav){
+        header.classList.add('navbar-fixed');
+    }else{
+        header.classList.remove('navbar-fixed');
+    }
+}
+
+// Hamburger
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+
+hamburger.addEventListener('click', function(){
+    hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
+});
+
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container:{
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors:{
+        primary: '#3b82f6',
+        dark: '#292524',
+        hover: '#0330fc',
+        github: '#000000',
+        instagram: '#ff75e5',
+        facebook: '#3658d6',
+        linkedin: '#3367d6',
+        twitter: '#1da1f2',
+      },
+      screens:{
+        'lg': '1024px',
+      }
+    },
+  },
+  plugins: [],
+}
